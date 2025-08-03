2e6fdc531e019bbe5e0b39b425c1938a6c33f16b
vastavikadi <aditya82004shukla@gmail.com>
2025-08-03 10:58:39 +0530

    void and never

--- function.ts
+++ function.ts
@@ -26,7 +26,55 @@ console.log(sumTwo(6,5));
 function sumThree(num1:number, num2:number,num3:number):number{
     return num1+num2+num3;
 }
+const getSumThree=(num1:number, num2:number,num3:number):number=>{//this is how u do it for arrow function
+    return num1+num2+num3;
+}
 
 console.log(sumThree(4,3,2));//it is not giving out any errors although we pass in 3 numbers expecting their sum to be returned but actually returning the string, to solve this and be confirmed about the datatype to be returned by the function simply define the datatype of the function as well. How its done: function sumThree(num1:number, num2:number,num3:number):number //its done
 
+
+//how to handle if we were to return more than one type. example below:
+function responseJson(val:number){
+    if(val>5)return true;
+    return "500 Internal Server Error";//here there are two types that are being returned depending on the condition but how to appedn these both (boolean and string as the types for the function)
+    //for this we use something called Union //later explained
+}
+
+
+const heros =["IronMan", "Thor","America's Ass"];
+
+const result = heros.map((hero:string):string=>{//now here we dont need to tell ts that the type of the hero is gonna be string because it is smart enough to know that (we can if we wnat additional security hero:string or (hero:string)) but surely need to define the type of return this makes and we do it like: (hero):string//this is the datatype of the function so by default the type is gonna be string[](array of strings) but we can manipulate
+    return `Our hero is ${hero}`
+})
+console.log(result);
+
+const numbers=[1,2,3,4]
+
+console.log(numbers.map((num:number):string=>{
+    return `num is ${num}`;
+}))
+
+
+//console log and handling errors
+function consoleError(errmsg:string): void{//Function (void) doesn’t return a value, but does finish executing	Logging to console, finishing a task
+    console.log(errmsg);//the function is not returning anything hence the type of the function should be void
+}
+
+function handleError(errmsg:string):never{//type for throwing errors will always be never because Function (never) returns at all — because it throws or loops forever	Throwing an error or infinite loop or crashes the program
+    throw new Error(errmsg)
+}
+
+//example
+function fail(message: string): never {
+  throw new Error(message);
+}
+
+function process(input: string) {
+  if (input === "") {
+    return fail("Input cannot be empty");
+  }
+
+  console.log("Processing:", input);
+}
+process("");
 export{}
