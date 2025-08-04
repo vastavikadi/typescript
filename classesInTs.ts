4cbc48c4606ef853a0727d08e07409c038159e92
vastavikadi <aditya82004shukla@gmail.com>
2025-08-04 11:19:47 +0530

    classes

--- classesInTs.ts
+++ classesInTs.ts
@@ -1,8 +1,10 @@
-class Users{
-    email:string;
-    name:string;//define the types of the inputs/details of Users and then use
-    constructor(email:string, name:string){//types of the input
-        this.email=email;
-        this.name=name;
-    }
+class Users {
+  email: string;
+  name: string; //define the types of the inputs/details of Users and then use
+  constructor(email: string, name: string) {
+    //types of the input
+    this.email = email;
+    this.name = name;
+  }
 }
+const Amisha = new Users("Amisha@gmail.com", "Amisha"); //new instance/object of the class Users
